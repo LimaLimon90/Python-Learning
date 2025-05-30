de85b3d535b1b109d0adf4f9674820af774c1234
Fernando Limon <64861804+LimaLimon90@users.noreply.github.com>
2025-05-30 12:13:01 -0700

    Add files via upload

---
+++ Tipos de datos/P2_Tipos_de_datos.py
@@ -0,0 +1,21 @@
+#Declaracion de las variables.
+Numero=8
+Palabra="Python"
+Boolean=False
+Flotante=3.14159265
+complejo=4+3j
+Lista=[1,2,3]
+Tupla=(4,5,6)
+Set={7,8,9}
+Diccionario={"Nombre":"Fernando"}
+
+#Impresion y clacificacion de los tipos de datos
+print("La variable Numero con valor",Numero, "es del tipo:",type(Numero))
+print("La variable Palabra con valor",Palabra, "es del tipo: ",type(Palabra))
+print("La variable Boolean con valor",Boolean, "es del tipo: ",type(Boolean))
+print("La variable Flotante con valor",Flotante, "es del tipo: ",type(Flotante))
+print("La variable Compleo con valor",complejo, "es del tipo: ",type(complejo))
+print("La variable Lista con valor",Lista, "es del tipo: ",type(Lista))
+print("La variable Tupla con valor",Tupla, "es del tipo: ",type(Tupla))
+print("La variable Set con valor",Set, "es del tipo: ",type(Set))
+print("La variable Diccionario con valor",Diccionario, "es del tipo: ",type(Diccionario))
