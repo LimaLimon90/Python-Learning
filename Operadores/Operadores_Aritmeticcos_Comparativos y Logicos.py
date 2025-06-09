773e4663534d1be054a91efc97c4bbee9a3ea577
Fernando Limon <64861804+LimaLimon90@users.noreply.github.com>
2025-06-09 10:54:12 -0700

    Create Operadores_Aritmeticcos_Comparativos y Logicos.py

---
+++ Operadores/Operadores_Aritmeticcos_Comparativos y Logicos.py
@@ -0,0 +1,91 @@
+#Operadores Aritmeticos, comparadores y logicos
+
+#Operadores Aritmeticos
+num=2
+x=num
+print("Para x=2")
+#Adicion
+print("Adicion x+2:")
+x+=2
+print(x)
+
+#Sustraccion
+x=num
+print("Sustraccion x-1:")
+x-=1
+print(x)
+
+#Multiplicacion
+x=num
+print("Multiplicacion x*4:")
+x*=4
+print(x)
+
+#Divicion
+x=num
+print("Divicion x/2:")
+x/=2
+print(x)
+
+#Modulo
+x=num
+print("Modulo x%4:")
+x%=4
+print(x)
+
+#Division de piso (Devuelve el entero menor mas cervano a la divicion)
+x=num
+print("Divicion de piso x//3:")
+x//=3
+print(x)
+
+#Exponente
+x=num
+print("Exponente x^5:")
+x**=5
+print(x)
+
+print("")
+
+#Operadores Comparativos
+print("Para a=5 y b=8")
+a=5
+b=8
+
+#Menor que
+print("a es menor que b:")
+print(a<b)
+
+#Mayor que
+print("a es mayor que b:")
+print(a>b)
+
+#Menor o igual que
+print("a es menor o igual que b:")
+print(a<=b)
+
+#Mayor que
+print("a es mayor o igual que b:")
+print(a>=b)
+
+#Igual que
+print("a es igual que b:")
+print(a==b)
+
+#DIferente de
+print("a es diferente de b:")
+print(a!=b)
+
+print("")
+#Operadores Logicos
+#Operador Y
+print("true y False es:")
+print(True and False)
+
+#Operador O
+print("True o False es:")
+print(True or False)
+
+#Operador No
+print("No false es:")
+print(not False)
